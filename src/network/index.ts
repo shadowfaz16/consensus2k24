ca7782b40bdb14216e1478ed0cf74e60c7f3796b
William Blankenship <retrohacker@dugtrio.dugtrio>
2024-05-29 13:20:35 -0600

    aggressive DHT connections from browser tab

---
+++ src/network/index.ts
@@ -0,0 +1,159 @@
+import { kadDHT, removePrivateAddressesMapper } from "@libp2p/kad-dht";
+import {
+  createDelegatedRoutingV1HttpApiClient,
+  DelegatedRoutingV1HttpApiClient,
+} from "@helia/delegated-routing-v1-http-api-client";
+import { createLibp2p, Libp2p } from "libp2p";
+import { identify } from "@libp2p/identify";
+import { peerIdFromString } from "@libp2p/peer-id";
+import { noise } from "@chainsafe/libp2p-noise";
+import { yamux } from "@chainsafe/libp2p-yamux";
+import first from "it-first";
+import { bootstrap } from "@libp2p/bootstrap";
+import { Multiaddr } from "@multiformats/multiaddr";
+import type {
+  Connection,
+  Message,
+  SignedMessage,
+  PeerId,
+} from "@libp2p/interface";
+import { webSockets } from "@libp2p/websockets";
+import { webTransport } from "@libp2p/webtransport";
+import { webRTC, webRTCDirect } from "@libp2p/webrtc";
+import { circuitRelayTransport } from "@libp2p/circuit-relay-v2";
+
+const WEBRTC_BOOTSTRAP_PEER_ID =
+  "12D3KooWGahRw3ZnM4gAyd9FK75v4Bp5keFYTvkcAwhpEm28wbV3";
+const WEBTRANSPORT_BOOTSTRAP_PEER_ID =
+  "12D3KooWFhXabKDwALpzqMbto94sB7rvmZ6M28hs9Y9xSopDKwQr";
+const BOOTSTRAP_PEER_IDS = [
+  WEBTRANSPORT_BOOTSTRAP_PEER_ID,
+  WEBRTC_BOOTSTRAP_PEER_ID,
+];
+
+export class Network {
+  libp2p: Libp2p | undefined;
+  ready(): boolean {
+    return this.libp2p !== undefined;
+  }
+  async init() {
+    // Enable verbose logging for debugging
+    localStorage.debug = "ui*,libp2p*,-libp2p:connection-manager*,-*:trace";
+
+    const delegatedClient = createDelegatedRoutingV1HttpApiClient(
+      "https://delegated-ipfs.dev",
+    );
+
+    const { bootstrapAddrs, relayListenAddrs } =
+      await getBootstrapMultiaddrs(delegatedClient);
+
+    this.libp2p = await createLibp2p({
+      addresses: {
+        listen: [
+          // Listen for webRTC connection
+          "/webrtc",
+          // TODO: Use the app's bootstrap nodes as circuit relays
+          ...relayListenAddrs,
+        ],
+      },
+      transports: [
+        webTransport(),
+        webSockets(),
+        webRTC({
+          rtcConfiguration: {
+            iceServers: [
+              {
+                // STUN servers help the browser discover its own public IPs
+                urls: [
+                  "stun:stun.l.google.com:19302",
+                  "stun:global.stun.twilio.com:3478",
+                ],
+              },
+            ],
+          },
+        }),
+        // Required to estalbish connections with peers supporting WebRTC-direct, e.g. the Rust-peer
+        webRTCDirect(),
+        // Required to create circuit relay reservations in order to hole punch browser-to-browser WebRTC connections
+        circuitRelayTransport({
+          // When set to >0, this will look up the magic CID in order to discover circuit relay peers it can create a reservation with
+          discoverRelays: 2,
+        }),
+      ],
+      connectionManager: {
+        maxConnections: 30,
+        minConnections: 5,
+      },
+      connectionEncryption: [noise()],
+      streamMuxers: [yamux()],
+      connectionGater: {
+        denyDialMultiaddr: async () => false,
+      },
+      peerDiscovery: [
+        bootstrap({
+          // The app-specific go and rust bootstrappers use WebTransport and WebRTC-direct which have ephemeral multiadrrs
+          // that are resolved above using the delegated routing API
+          list: bootstrapAddrs,
+        }),
+      ],
+      services: {
+        // Delegated routing helps us discover the ephemeral multiaddrs of the dedicated go and rust bootstrap peers
+        // This relies on the public delegated routing endpoint https://docs.ipfs.tech/concepts/public-utilities/#delegated-routing
+        delegatedRouting: () => delegatedClient,
+        identify: identify(),
+        dht: kadDHT({
+          protocol: "/ipfs/kad/1.0.0",
+          peerInfoMapper: removePrivateAddressesMapper,
+        }),
+      },
+    });
+  }
+}
+
+async function getBootstrapMultiaddrs(
+  client: DelegatedRoutingV1HttpApiClient,
+): Promise<BootstrapsMultiaddrs> {
+  const peers = await Promise.all(
+    BOOTSTRAP_PEER_IDS.map((peerId) =>
+      first(client.getPeers(peerIdFromString(peerId))),
+    ),
+  );
+
+  const bootstrapAddrs = [];
+  const relayListenAddrs = [];
+  for (const p of peers) {
+    if (p && p.Addrs.length > 0) {
+      for (const maddr of p.Addrs) {
+        const protos = maddr.protoNames();
+        if (
+          (protos.includes("webtransport") ||
+            protos.includes("webrtc-direct")) &&
+          protos.includes("certhash")
+        ) {
+          if (maddr.nodeAddress().address === "127.0.0.1") continue; // skip loopback
+          bootstrapAddrs.push(maddr.toString());
+          relayListenAddrs.push(getRelayListenAddr(maddr, p.ID));
+        }
+      }
+    }
+  }
+  return { bootstrapAddrs, relayListenAddrs };
+}
+
+interface BootstrapsMultiaddrs {
+  // Multiaddrs that are dialable from the browser
+  bootstrapAddrs: string[];
+
+  // multiaddr string representing the circuit relay v2 listen addr
+  relayListenAddrs: string[];
+}
+
+// Constructs a multiaddr string representing the circuit relay v2 listen address for a relayed connection to the given peer.
+const getRelayListenAddr = (maddr: Multiaddr, peer: PeerId): string =>
+  `${maddr.toString()}/p2p/${peer.toString()}/p2p-circuit`;
+
+export const getFormattedConnections = (connections: Connection[]) =>
+  connections.map((conn) => ({
+    peerId: conn.remotePeer,
+    protocols: Array.from(new Set(conn.remoteAddr.protoNames())),
+  }));
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,15 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import { Button } from "@/components/ui/button";
 import Connectbutton from "@/components/thirdweb/ConnectButton";
-import Balance from "@/components/thirdweb/BalanceOf"
+import Balance from "@/components/thirdweb/BalanceOf";
+import { Network } from "@/network";
 
 const inter = Inter({ subsets: ["latin"] });
+
+(async () => {
+  let network = new Network();
+  network.init();
+})();
 
 export default function Home() {
   return (
@@ -19,11 +25,11 @@ export default function Home() {
         <Button className="bg-red-900 dark:bg-white">Click me</Button>
         <Connectbutton />
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-        <div>
-          BALANCE OF
-          <Balance />
+          <div>
+            BALANCE OF
+            <Balance />
           </div>
-          </div>
+        </div>
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
